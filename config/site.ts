4646bceb635c418bbaf2b8a5b1b9762efb7783a9
Medved Dima <kosolapiyd@gmail.com>
2025-06-11 15:52:43 +0300

    metadata config

---
+++ config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  name: 'Taskify',
+  description:
+    'Taskify is a task management system that helps teams collaborate, manage projects, and reach new productivity peaks.',
+  //   url: 'https://taskify.com',
+};
