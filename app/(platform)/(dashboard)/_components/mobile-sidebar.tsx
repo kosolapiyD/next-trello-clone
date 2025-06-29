8293b3ef716be9db20cc7561f6ccda2e52dbae4f
Medved Dima <kosolapiyd@gmail.com>
2025-06-29 16:16:56 +0300

    mobile sidebar init

---
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { Menu } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
+
+// import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
+import { Button } from '@/components/ui/button';
+// import { Sheet, SheetContent } from "@/components/ui/sheet";
+
+import { Sidebar } from './sidebar';
+
+export const MobileSidebar = () => {
+  return <div>mobile-sidebar</div>;
+};
