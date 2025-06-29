4f9ba279e0a5463f80b7d944c96959d7efdfadb5
Medved Dima <kosolapiyd@gmail.com>
2025-06-29 16:08:01 +0300

    skeleton added to sidebar accordion nav-items

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -104,3 +104,14 @@ export const NavItem = ({
     </AccordionItem>
   );
 };
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className='flex items-center gap-x-2'>
+      <div className='w-10 h-10 relative shrink-0'>
+        <Skeleton className='h-full w-full absolute' />
+      </div>
+      <Skeleton className='h-10 w-full' />
+    </div>
+  );
+};
